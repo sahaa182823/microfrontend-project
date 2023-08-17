42817383272dd06cfc3ebd9a1f1691e5de59ed10
Anirban Saha <sahaa182823@gmail.com>
2023-08-17 20:29:36 +0530

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -21,9 +20,7 @@ const devConfig = {
             //shared: ['react', 'react-dom'], evrytime if you dont want to make entry of shared dependencies follow below syntax
             shared: packageJson.dependencies,  // this also reduce memory consumpsion in browser caused by js
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+       
     ]
 };
 
