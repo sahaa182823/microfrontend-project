9bc088bfb0c309e7da3a4defeb34014bedb44715
Anirban Saha <sahaa182823@gmail.com>
2023-08-19 23:46:06 +0530

    changed dev config file in marketing

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -17,7 +17,7 @@ const devConfig = {
             name: 'marketing',
             filename: 'remoteEntry.js',
             exposes: {
-                './MarketingApp': './src/bootstrap'
+                './Marketing': './src/bootstrap'
             },
             //shared: ['react','react-dom'],
             shared: packageJson.dependencies,
